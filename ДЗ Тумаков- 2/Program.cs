2ac82f7601afae3e856e1d9d64997f5590cbf018
samat <samat@SAMATGG>
2023-09-22 00:34:39 +0300

    Добавьте файлы проекта.

---
+++ ДЗ Тумаков- 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_Тумаков__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Упражнение 2.1 - Имя пользователя и приветствие
+            Console.WriteLine("Упр 2.1 - Имя пользователя и приветствие");
+            Console.WriteLine("Введите свое имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + "!");
+
+            // Упражнение 2.2 - Два числа, результат деления, исключение - деление на ноль
+            Console.WriteLine("Упр 2.2 - Два числа, результат деления, исключение - деление на ноль");
+            Console.WriteLine("Введите первое число");
+            double firstvalue = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double secondvalue = double.Parse(Console.ReadLine());
+            double result = firstvalue / secondvalue;
+            if (secondvalue != 0)
+            {
+                Console.WriteLine("Результат деления чисел = " + result);
+            }
+            else
+            {
+                Console.WriteLine("Число нельзя делить на ноль!");
+            }
+        }
+    }
+}
---
+++ ДЗ Тумаков- 2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ДЗ Тумаков- 2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ДЗ Тумаков- 2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("094fd6e1-47a8-423b-9c31-404e9640d678")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
